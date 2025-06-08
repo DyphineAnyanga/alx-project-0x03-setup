da2d4d9d22a64310513ce21a5ca9e987909fe55e
dyphine anyanga <dyphineanyanga@gmail.com>
2025-06-09 00:58:59 +0300

    Initial setup of alx-project-0x00 with typescript, tailwind, eslint, routing, and components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import { PageRouteProps } from "@/interface";
+import React from "react";
 
-const Home = ({ pageRoute }: PageRouteProps) => {
+const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to {pageRoute}</h1>
-    </div>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
   );
 };
 
