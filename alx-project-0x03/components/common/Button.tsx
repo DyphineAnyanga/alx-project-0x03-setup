451508d242585c366dac19978c139f4a41eee77b
dyphine anyanga <dyphineanyanga@gmail.com>
2025-06-13 20:44:02 +0300

    refactor: move interfaces to interface/index.ts for better code organization

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,14 +1,23 @@
-interface ButtonProps {
-  action: () => void;
-  buttonLabel: string;
-  buttonBackgroundColor: string;
-}
+// components/common/Button.tsx
 
-const Button = ({ action, buttonLabel, buttonBackgroundColor }: ButtonProps) => {
+import React from "react";
+import { ButtonProps } from "@/interface";
+
+const Button: React.FC<ButtonProps> = ({
+  action,
+  buttonLabel,
+  buttonBackgroundColor,
+}) => {
+  const bgColor = {
+    blue: "bg-blue-500 hover:bg-blue-600",
+    green: "bg-green-500 hover:bg-green-600",
+    orange: "bg-orange-500 hover:bg-orange-600",
+  }[buttonBackgroundColor] || "bg-gray-500 hover:bg-gray-600";
+
   return (
     <button
       onClick={action}
-      className={`px-4 py-2 rounded text-white bg-${buttonBackgroundColor}-500 hover:bg-${buttonBackgroundColor}-600`}
+      className={`text-white font-semibold py-2 px-4 rounded ${bgColor} transition duration-300`}
     >
       {buttonLabel}
     </button>
